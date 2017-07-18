fbc6740d862f10ecb582916a0c200375ab3788de
calebcox725 <caleb.cox@flatironschool.com>
2017-07-18 11:54:11 -0400

    Add ESLint rule to allow tagged template expressions (for styled-components)

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
     'import'
   ],
   'rules': {
-    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }]
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
+    'no-unused-expressions': [1, { 'allowTaggedTemplates': true }]
   },
   'env': {
     'browser': true,
